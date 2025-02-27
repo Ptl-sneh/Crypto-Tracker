550776a03134f6eee4ca4b11f7a4c8e63c749545
Ptl-sneh <156177898+Ptl-sneh@users.noreply.github.com>
2025-02-28 00:59:06 +0530

    Delete try1.py

--- try1.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-url = "https://api.coingecko.com/api/v3/coins/markets"
-params = {"vs_currency": "inr", "order": "market_cap_desc", "per_page": "100", "page": "1", "sparkline": "false"}
-response = requests.get(url,params=params)
-data = response.json()
-print(data)
-for coin in data:
-    print(coin['name'])
